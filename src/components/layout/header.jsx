3fb57eee223e20d845872de5d65ec57b95289a27
mquang279 <quangm279@gmail.com>
2025-08-16 10:38:56 +0700

    fix: header width

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -10,7 +10,7 @@ const NAV_ITEMS = [
 const Header = () => {
     return (
         <div className="header w-full">
-            <div className="container sticky top-0 bg-[linear-gradient(90deg,rgba(20,20,20,1)_55%,rgba(110,21,25,1)_100%,rgba(0,212,255,1)_100%)] text-white w-full h-[70px] flex justify items-center px-[30px] border-b-[0.5px] border-gray-600">
+            <div className="sticky top-0 bg-[linear-gradient(90deg,rgba(20,20,20,1)_55%,rgba(110,21,25,1)_100%,rgba(0,212,255,1)_100%)] text-white w-full h-[70px] flex justify-between items-center px-[30px] border-b-[0.5px] border-gray-600">
                 <NavLink to="/" className="header-logo pr-[36px]">
                     <img src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png" alt="logo" className="h-auto w-32" />
                 </NavLink>
@@ -23,8 +23,8 @@ const Header = () => {
                         )}
                     </ul>
                 </div>
-                <div className="header-action flex text-nowrap font-medium gap-8">
-                    <NavLink className="w-full hover:underline cursor-pointer">For Employers</NavLink>
+                <div className="header-action flex items-center text-nowrap font-medium gap-8 flex-shrink-0">
+                    <NavLink to="/employers" className="hover:underline cursor-pointer">For Employers</NavLink>
                     <NavLink to="/login" className="w-full hover:underline cursor-pointer">Sign in/Sign up</NavLink>
                 </div>
             </div>
