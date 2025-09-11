dd0f67062f0527b1852f8e8bad1f8ee3de082528
mquang279 <quangm279@gmail.com>
2025-09-11 21:06:56 +0700

    feat: add responsive for header

--- src/components/layout/default/Header.jsx
+++ src/components/layout/default/Header.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router"
 import { useAuth } from "../../../providers/AuthProvider"
+import { Sidebar, SideBarItem } from "../../ui/Sidebar"
+import { BriefcaseBusiness, Building2, FileUser, LayoutDashboard, NotebookText } from "lucide-react"
 
 const NAV_ITEMS = [
-    { name: 'All Jobs', link: '/' },
-    { name: 'IT Companies', link: '/companies' },
-    { name: 'Blog', link: '/blogs' },
-    { name: 'CV IT Templates', link: '/templates' }
+    { name: 'All Jobs', link: '/', icon: <BriefcaseBusiness /> },
+    { name: 'IT Companies', link: '/companies', icon: <Building2 /> },
+    { name: 'Blog', link: '/blogs', icon: <NotebookText /> },
+    { name: 'CV IT Templates', link: '/templates', icon: <FileUser /> }
 ]
 
 const Header = () => {
@@ -22,10 +24,27 @@ const Header = () => {
     return (
         <div className="header w-full sticky top-0 z-50">
             <div className="bg-[linear-gradient(90deg,rgba(20,20,20,1)_55%,rgba(110,21,25,1)_100%,rgba(0,212,255,1)_100%)] text-white w-full h-[70px] flex justify-between items-center px-[30px] border-b-[0.5px] border-gray-600">
+                {/* <Sidebar /> */}
+                <Sidebar>
+                    <>
+                        {NAV_ITEMS.map((item, index) => {
+                            return (
+                                <SideBarItem
+                                    key={index}
+                                    icon={item.icon}
+                                    text={item.name}
+                                />)
+                        })}
+                    </>
+                </Sidebar>
+
+                {/* Logo */}
                 <NavLink to="/" className="header-logo pr-[36px]">
-                    <img src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png" alt="logo" className="h-auto w-32" />
+                    <img src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png" alt="logo" className="h-8 md:h-12 w-32 object-contain" />
                 </NavLink>
-                <div className="header-nav w-full flex">
+
+                {/* Nav items */}
+                <div className="hidden lg:flex header-nav w-full">
                     <ul className="flex w-full gap-9 font-medium text-[rgb(166,166,166)]">
                         {NAV_ITEMS.map((item, index) =>
                             <li key={index} className="hover:text-white cursor-pointer">
@@ -34,8 +53,10 @@ const Header = () => {
                         )}
                     </ul>
                 </div>
+
+                {/* User */}
                 <div className="header-action flex items-center text-nowrap font-medium gap-8 flex-shrink-0">
-                    <NavLink to="/employers" className="hover:underline cursor-pointer">For Employers</NavLink>
+                    <NavLink to="/employers" className="hidden lg:flex hover:underline cursor-pointer">For Employers</NavLink>
                     {!isAuthenticated &&
                         <NavLink to="/login" className="w-full hover:underline cursor-pointer">Sign in/Sign up</NavLink>
                     }
@@ -45,11 +66,11 @@ const Header = () => {
                                 <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center text-sm font-semibold">
                                     {user.username?.charAt(0).toUpperCase() || 'U'}
                                 </div>
-                                <span className="text-white">{user.username || user.email}</span>
+                                <span className="hidden lg:flex text-white">{user.username || user.email}</span>
                             </div>
                             <button
                                 onClick={handleLogout}
-                                className="hover:underline cursor-pointer text-white"
+                                className="hidden lg:flex hover:underline cursor-pointer text-white"
                             >
                                 Logout
                             </button>
---
+++ src/components/ui/Sidebar.jsx
@@ -0,0 +1,37 @@
+import { ChevronFirst, ChevronLast, ClosedCaption, SidebarClose } from "lucide-react"
+import { useState } from "react"
+
+const Sidebar = ({ children }) => {
+    const [expanded, setExpanded] = useState(false)
+
+    return (
+        !expanded ? <ChevronFirst onClick={() => setExpanded(true)} /> :
+            <aside className={`h-screen lg:hidden fixed top-0 left-0 z-50`}>
+                <nav className="h-full w-full flex flex-col bg-black shadow-sm">
+                    <div className="p-4 pb-2 flex justify-end items-center">
+                        <img src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png" alt="logo" className="h-8 md:h-12 w-32 object-contain" />
+                        <button onClick={() => setExpanded(false)} className="p-1.5 rounded-lg text-white hover:text-red-600">
+                            <SidebarClose />
+                        </button>
+                    </div>
+
+                    {expanded &&
+                        <ul className="flex-1 px-3 mt-4 text-white font-medium">
+                            {children}
+                        </ul>
+                    }
+                </nav>
+            </aside>
+    )
+}
+
+const SideBarItem = ({ icon, text }) => {
+    return (
+        <li className=" flex relative items-center py-2 px-2 gap-3 rounded-lg hover:bg-red-600">
+            {icon}
+            <span>{text}</span>
+        </li >
+    )
+}
+
+export { Sidebar, SideBarItem }
