a4a2d6bf241406b26378148d906ac2f1b149db66
mquang279 <quangm279@gmail.com>
2025-09-14 09:04:33 +0700

    chore: modifying routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,9 +4,10 @@ import AdminPage from "../pages/admin/AdminPage";
 import AdminJobsPage from "../pages/admin/AdminJobsPage";
 import AdminCompaniesPage from "../pages/admin/AdminCompaniesPage";
 import HomePage from "../pages/home/Home";
-import LoginPage from "../pages/login/LoginPage";
 import JobPage from "../pages/job/JobPage";
 import CompanyPage from "../pages/company/CompanyPage";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
 
 const publicRoutes = [
     { path: '/', component: HomePage, layout: DefaultLayout },
@@ -15,7 +16,8 @@ const publicRoutes = [
     { path: '/admin', component: AdminPage, layout: AdminLayout },
     { path: '/admin/jobs', component: AdminJobsPage, layout: AdminLayout },
     { path: '/admin/companies', component: AdminCompaniesPage, layout: AdminLayout },
-    { path: '/companies/:id', component: CompanyPage, layout: DefaultLayout }
+    { path: '/companies/:id', component: CompanyPage, layout: DefaultLayout },
+    { path: '/register', component: RegisterPage, layout: DefaultLayout }
 ]
 
 const privateRoutes = [
